26984d8c3ce8a7013eeb65a12ad2faeb94862fd3
it21156960 <chamith227@gmail.com>
2024-04-28 23:42:45 +0530

    userServiceAdded

---
+++ user-service/src/main/java/com/skillsprint/userservice/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.skillsprint.userservice.controller;
+
+import com.skillsprint.userservice.DTO.UserDTO;
+import com.skillsprint.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("api/v1/users")
+@RestController
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable String userId) {
+        return ResponseEntity.ok(userService.getUserById(userId));
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUserById(@PathVariable String userId) {
+        userService.deleteUserById(userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDTO> updateUserById(@PathVariable String userId, @RequestBody UserDTO updatedUserDTO) {
+        UserDTO updatedUser = userService.updateUserById(userId, updatedUserDTO);
+        if (updatedUser != null) {
+            return ResponseEntity.ok(updatedUser);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
