5312cdebf22dd21d3f2fd149a38442076cc65b3a
IsuruX98 <isurusanka98@gmail.com>
2024-05-07 22:26:46 +0530

    application fixed

--- api-gateway/src/main/java/com/skillsprint/apigateway/config/AppConfig.java
+++ api-gateway/src/main/java/com/skillsprint/apigateway/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.skillsprint.apigateway.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class AppConfig {
 
     @Bean
+    @LoadBalanced
     public RestTemplate  template(){
         return new RestTemplate();
     }
