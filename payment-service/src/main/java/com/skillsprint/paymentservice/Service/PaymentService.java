1f1abd2e34de938f09e59857eab0bcb6dbf7f700
conscience-development <it21164330@my.sliit.lk>
2024-05-13 04:25:02 +0530

    paymentDone

---
+++ payment-service/src/main/java/com/skillsprint/paymentservice/Service/PaymentService.java
@@ -0,0 +1,14 @@
+package com.skillsprint.paymentservice.Service;
+
+import com.skillsprint.paymentservice.DTO.PaymentResDTO;
+
+import java.util.List;
+
+public interface PaymentService {
+
+    PaymentResDTO addPayment(PaymentResDTO dto);
+    List<PaymentResDTO> getAllPaymentsForCourse(String courseId);
+    List<PaymentResDTO> getAllPaymentsByStudent(String studentId);
+    PaymentResDTO getPaymentByCourseIdUserID(String courseId,String studentId);
+    PaymentResDTO getPaymentById(int paymentId);
+}
