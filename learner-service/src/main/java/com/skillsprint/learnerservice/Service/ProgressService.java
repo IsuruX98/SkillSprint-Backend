ed42101aee027d63cf24b09bed8630d891357867
conscience-development <it21164330@my.sliit.lk>
2024-05-12 20:35:53 +0530

    progressUpdateChange!!

--- learner-service/src/main/java/com/skillsprint/learnerservice/Service/ProgressService.java
+++ learner-service/src/main/java/com/skillsprint/learnerservice/Service/ProgressService.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface ProgressService {
     ProgressDTO createProgress(ProgressDTO progressDTO);
     ProgressDTO getProgressById(String id);
-    ProgressDTO updateProgress(String id,int indexModule);
+    ProgressDTO updateProgress(String userId, String courseId, int indexModule);
     void deleteProgressById(String id);
     List<ProgressDTO> getAllProgresses();
 }
--- learner-service/src/main/java/com/skillsprint/learnerservice/repository/ProgressRepository.java
+++ learner-service/src/main/java/com/skillsprint/learnerservice/repository/ProgressRepository.java
@@ -4,6 +4,9 @@ import com.skillsprint.learnerservice.model.Progress;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProgressRepository extends MongoRepository<Progress, String> {
+    Optional<Object> findByUserIdAndCourseId(String userId, String courseId);
 }
