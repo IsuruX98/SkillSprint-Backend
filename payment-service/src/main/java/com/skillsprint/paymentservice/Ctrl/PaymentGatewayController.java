945a4608e8cb653b9a525b10d4e7725c7c595873
conscience-development <it21164330@my.sliit.lk>
2024-05-13 04:23:23 +0530

    PaymentUpdated

--- payment-service/src/main/java/com/skillsprint/paymentservice/Ctrl/PaymentGatewayController.java
+++ payment-service/src/main/java/com/skillsprint/paymentservice/Ctrl/PaymentGatewayController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin("*")
 @RequestMapping("/api/v1/payment")
 public class PaymentGatewayController {
     private StripeClient stripeClient;
