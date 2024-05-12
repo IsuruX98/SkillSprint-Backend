29ee95e0bb5d20c0d825d4dfc425b9fe87827ee7
it21156960 <chamith227@gmail.com>
2024-05-12 19:52:36 +0530

    responseDTO_updated

---
+++ content-service/src/main/java/com/example/contentservice/dto/ResponseDTO.java
@@ -0,0 +1,14 @@
+package com.example.contentservice.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDTO {
+    private String message;
+    private Object content;
+}
