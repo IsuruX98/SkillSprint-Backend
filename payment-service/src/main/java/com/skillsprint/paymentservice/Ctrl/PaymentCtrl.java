d3339ba14fd0b91cf98260fe954806fd864e9040
conscience-development <it21164330@my.sliit.lk>
2024-05-13 05:07:31 +0530

    paymentDoneNDusted

--- payment-service/src/main/java/com/skillsprint/paymentservice/Ctrl/PaymentCtrl.java
+++ payment-service/src/main/java/com/skillsprint/paymentservice/Ctrl/PaymentCtrl.java
@@ -4,15 +4,16 @@ package com.skillsprint.paymentservice.Ctrl;
 import com.skillsprint.paymentservice.DTO.PaymentResDTO;
 import com.skillsprint.paymentservice.Service.PaymentService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@CrossOrigin("*")
 @RequestMapping("/api/v1/payment-details")
 @RequiredArgsConstructor
+@Slf4j
 public class PaymentCtrl {
     private final PaymentService paymentService;
 
@@ -39,8 +40,10 @@ public class PaymentCtrl {
         PaymentResDTO payment = paymentService.getPaymentByCourseIdUserID(courseId, studentId);
 
         if (payment != null) {
+            log.info("Done Payment");
             return ResponseEntity.ok(payment);
         } else {
+            log.info("Error in payment");
             return ResponseEntity.notFound().build();
         }
     }
