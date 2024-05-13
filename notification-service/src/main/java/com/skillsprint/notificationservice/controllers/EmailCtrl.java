5095844cbd12de227f9f386e2715c4f1f9d790e9
it21156960 <chamith227@gmail.com>
2024-05-13 19:28:58 +0530

    emailServiceUpdated

--- notification-service/src/main/java/com/skillsprint/notificationservice/controllers/EmailCtrl.java
+++ notification-service/src/main/java/com/skillsprint/notificationservice/controllers/EmailCtrl.java
@@ -1,9 +1,8 @@
 package com.skillsprint.notificationservice.controllers;
 
 import com.skillsprint.notificationservice.DTO.EmailBodyDTO;
-import com.skillsprint.notificationservice.service.EmailSendService;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import com.skillsprint.notificationservice.service.EmailService;
+import com.skillsprint.notificationservice.service.ServiceImpl.EmailSendService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/email") // Endpoint for mail sending
 public class EmailCtrl {
-    private  EmailSendService emailSendService; // Service dependency injection
+    private EmailService emailSendService; // Service dependency injection
     @PostMapping("/send-email")
     public ResponseEntity sendEmail(@RequestBody EmailBodyDTO dto){
         emailSendService.sendEmail(dto.getTo(), dto.getSubject(), dto.getMsg()); //call sendEMail method and set parameters from request body
